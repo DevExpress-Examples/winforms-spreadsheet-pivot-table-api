a9954caa49fe67093946258bcd6067ef23d4c7b3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SpreadsheetPivotTableExamples/CodeUtils/SpreadsheetExampleEvaluatorByTimer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetPivotTableExamples
+{
+    #region SpreadsheetExampleEvaluatorByTimer
+    public class SpreadsheetExampleEvaluatorByTimer : ExampleEvaluatorByTimer
+    {
+        public SpreadsheetExampleEvaluatorByTimer()
+            : base()
+        {
+        }
+
+        protected override ExampleCodeEvaluator GetExampleCodeEvaluator(ExampleLanguage language)
+        {
+            if (language == ExampleLanguage.VB)
+                return new SpreadsheetVbExampleCodeEvaluator();
+            return new SpreadsheetCSExampleCodeEvaluator();
+        }
+    }
+    #endregion
+}
